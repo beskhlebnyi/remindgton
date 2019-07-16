8d8c75725f2f086aadaa533d3ed740a127c222a5
beskhlebnyi <yaroslav@chimplie.com>
2019-07-16 22:10:32 +0300

    add multithreading supporting

--- main.rb
+++ main.rb
@@ -5,11 +5,15 @@ token =  ENV['BOT_API_TOKEN']
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-    when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    Thread.start(message) do |message|
+      case message.text
+      when '/start'
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      when '/stop'
+        bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+      else '/map'
+        bot.api.send_message(chat_id: message.chat.id, text: "I have no idea what this command means")
+      end
     end
   end
 end
